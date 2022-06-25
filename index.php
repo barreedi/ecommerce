5271c7a08a32a48c96e886aa39415e953b8366f1
barreedi <costabarreiros@hotmail.com>
2022-06-25 20:53:22 -0300

    criamos pageadmin e tpl admin

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -20,6 +20,14 @@ $app->get('/', function() {
 	
 });
 
+$app->get('/admin', function() {
+
+	$page =new PageAdmin();
+
+	$page->setTpl("index");
+    
+	
+});
 $app->run();
 
  ?>
